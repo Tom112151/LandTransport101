d49bd467545cf0046ea3af4b133d06431c2a5867
Andreas001 <andreasgeraldo0@gmail.com>
2018-04-19 21:34:31 +0700

    Create app.r

---
+++ app.r
@@ -0,0 +1,35 @@
+#install necessary libraries if not currently installed yet
+#install.packages("shiny")
+#install.packages("plotly")
+#install.packages("shinyjs)
+#install.packages("leaflet")
+#install.packages("jsonlite")
+
+#load the necessary libraries
+library(shiny)
+library(plotly)
+library(shinyjs)
+library(leaflet)
+library(jsonlite)
+
+#UI
+#Sets up descriptions
+ui <- fluidPage( 
+  dashboardHeader(title = "Airplane tracker in 1 April 18:00 pm"),
+  sliderInput("planeCount","Amount of Planes",1,8,2),
+  leafletOutput("mymap") 
+)
+
+#Server
+server <- function(input, output) 
+{
+  library(jsonlite)
+  airPlanes = read.csv("AircraftList1April1800.csv")
+  
+  output$mymap <- renderLeaflet({
+    leaflet(data = airPlanes[1:input$planeCount,]) %>% addTiles() %>%
+      addMarkers(~Longtitude, ~Latitude, popup = ~as.character(Model), label=~as.character(Airlines))
+})
+
+}
+shinyApp(ui, server)
