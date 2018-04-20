a7e3a2c0e1a2a5cb59bdb2bcba663d18495b5ba2
Andreas001 <andreasgeraldo0@gmail.com>
2018-04-20 08:57:02 +0700

    Update app.r

--- app.r
+++ app.r
@@ -1,50 +1,69 @@
-#Still currently in testing
+#Still currently in testing and not in any way finished
 
-#install necessary libraries if not currently installed yet
+#When using this for your own use it's better to retype it than copy and paste so i highly suggest it
+
+#Before you do anything you will need to install a few packages in order for things to work
+#All you need to do is type these thing's below (Without the #) in the R console
 #install.packages("shiny")
 #install.packages("shinyjs)
 #install.packages("leaflet")
 #install.packages("jsonlite")
 #install.packages("shinydashboard")
-#install.packages("rjson")
 
-#load the necessary libraries
+#You will also need to load the libraries by typing these in the console then you will finally be able to use it
 library(shiny)
 library(shinyjs)
 library(leaflet)
 library(jsonlite)
 library(shinydashboard)
 
-#UI
-#Sets up descriptions
+#UI-------------------------------------------------------------------------------------------------------------------------------------
+#Sets up the descriptions in the UI
+#You can change the parameters
+
+#the includeCSS is not a note, it will be needed to be there for things to run
+#Title is self explanatory
+#sliderInput is how much tracked items do you want to see (The 3 number parameters by order is minimum,maximum,default)
+#Leaflet creates the map
+
 ui <- fluidPage( 
-   # includeCSS("styles.css"),
-  dashboardHeader(title = "Airplane tracker in 1 April 18:00 pm"),
-  sliderInput("planeCount","Amount of Planes",1,8,2),
+  #includeCSS("styles.css"),
+  dashboardHeader(title = "Live tracker"),
+  sliderInput("count","Amount",1,8,2),
   leafletOutput("mymap") 
 )
 
-#Server
+#Server---------------------------------------------------------------------------------------------------------------------------------
+#
 server <- function(input, output, session) 
 {
+  #jsonlite loaded again to make sure
   library(jsonlite)
-  jsLandTransport = fromJSON("http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=33.433638&lng=-112.008113&fDstL=0&fDstU=100")
+  
+  #jsLandTransport will read from a json in a website and then landTransport will take jsLandTransport's information and put it into
+  #a data frame, the website json that is used for this is written below (The parameter for the website cannot be changed unless you
+  #make adjustment to the code)
+  jsLandTransport = fromJSON("http://api.metro.net/agencies/lametro/vehicles/")
   landTransport <- as.data.frame(jsLandTransport)
-  jsLandTransport
-   
+  
+  #Displaying the content of landTransport
+  landTransport
+  
+  #Yes
   values <- reactiveValues()
   
+  #This will make the map display showing the locations and details on each tracked land transports
+  output$mymap <- renderLeaflet({
+   leaflet(data = landTransport[1:input$count,]) %>% addTiles() %>%
+     addMarkers(~items.longtitude, ~items.latitude, popup = ~as.character(items.heading), label=~as.character(items.id))
+  
+  #Observes the API and make changes
   observe({
     invalidateLater(1000, session)
-    landTransport = fromJSON("http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=33.433638&lng=-112.008113&fDstL=0&fDstU=100")
+    landTransport = fromJSON("http://api.metro.net/agencies/lametro/vehicles/")
   })
    
 })
 
 }
 shinyApp(ui, server)
-#airPlanes = read.csv("AircraftList1April1800.csv")
-   
-  #output$mymap <- renderLeaflet({
-   # leaflet(data = airPlanes[1:input$planeCount,]) %>% addTiles() %>%
-     # addMarkers(~Longtitude, ~Latitude, popup = ~as.character(Model), label=~as.character(Airlines))
