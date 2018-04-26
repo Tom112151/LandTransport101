77c74b5e1371d9ab7aa0458343b2a65c704461f1
Andreas001 <andreasgeraldo0@gmail.com>
2018-04-26 20:40:47 +0700

    Update app.r

--- app.r
+++ app.r
@@ -59,7 +59,7 @@ server <- function(input, output, session)
   #This will make the map display showing the locations and details on each tracked land transports
   output$mymap <- renderLeaflet({
    leaflet(data = newDataFrame[1:input$count,]) %>% addTiles() %>%
-     addMarkers(~items.longtitude, ~items.latitude, popup = ~as.character(items.heading), label=~as.character(items.id))
+     addMarkers(~items.longitude, ~items.latitude, popup = ~as.character(items.heading), label=~as.character(items.id))
   
   #Observes the API and make changes
   #observe({
