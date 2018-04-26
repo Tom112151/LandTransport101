752ac68fcd9db379904d7820f52f958487928721
Thompson D.Y <thompsonjanely@gmail.com>
2018-04-27 01:27:07 +0700

    Create ui.r

---
+++ ui.r
@@ -0,0 +1,48 @@
+shinyUI
+(
+  dashboardPage
+  (
+    dashboardHeader
+    (
+      title = "DA WEI"
+    ),
+    
+    dashboardSidebar
+    (
+      menuItem("Dashboard"),
+        menuSubItem("sub board", tabName = "map"),
+      menuItem("new 1"),
+      menuItem("new 2")
+    ),
+    
+    dashboardBody
+    (
+      leafletOutput("map"),
+      tabItems
+      (
+        tabItem
+        (
+          tabName = "map",
+          box
+          (
+            title = "Heh",
+            collapsible = TRUE,
+            leafletOutput("map")
+          ),
+          box
+          (
+            title = "hah",
+            collapsible = TRUE,
+            sliderInput("count", "number", 1, 8, 2)
+          ),
+          box
+          (
+            title = "Heh",
+            collapsible = TRUE,
+            leafletOutput("plot")
+          )
+        )
+      )
+    )
+  )
+)
