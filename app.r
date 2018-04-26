d7eef4f7fd7b55abc50f5af2e1147d7dd08fd38f
Thompson D.Y <thompsonjanely@gmail.com>
2018-04-27 04:50:06 +0700

    Update app.r

--- app.r
+++ app.r
@@ -62,9 +62,9 @@ server <- function(input, output, session)
   
   #This will make the map display showing the locations and details on each tracked land transports
   output$mymap <- renderLeaflet({
-   leaflet(data = newDataFrame[1:input$count,]) %>% addTiles() %>%
+   leaflet(data = newDataFrame[1:input$count,], group = "Vehicles") %>% addTiles() %>%
      addMarkers(~items.longitude, ~items.latitude, icon = greenMarker)
-    leaflet(data2 = dataFrame2[1:input$count,]) %>% addTiles() %>%
+    leaflet(data = dataFrame2[1:input$count,],group = "Stop") %>% addTiles() %>%
      addMarkers(~items.longitude, ~items.latitude, icon = redMarker)
      
 })
