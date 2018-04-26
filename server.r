0af35956afd71382ef096ba181c115d97373ca6e
Thompson D.Y <thompsonjanely@gmail.com>
2018-04-27 01:21:35 +0700

    Update server.r

--- server.r
+++ server.r
@@ -11,18 +11,17 @@ shinyServer(function(input, output)
   dataFrame <- as.data.frame(jsonFile)
   newDataFrame <- dataFrame[c(3,4,6,7)]
   
+  jsonFile2 = fromJSON("http://api.metro.net/agencies/lametro/routes/704/stops/")
+  dataFrame2 <- as.data.frame(jsonFile2)
+  stopsDataFrame <- dataFrame2[c(1,2,3)]
+  
+  
   output$map <- renderLeaflet
   ({
-      leaflet(data = newDataFrame[1:input$count,]) 
-      leaflet() %>% addTiles() %>%
+      leaflet(data = newDataFrame[1:input$count,]) %>% addTiles() %>%
       addMarkers(~items.longitude, ~items.latitude, popup = ~as.character(items.heading), label =~as.character(items.id)) %>%
       points(items.longitude, items.latitude, col = "red", cex = .6)
     
-  })
-  
-  output$plot <- renderPlot
-  ({
-      newDataFrame(items.heading$items.id, breaks = input$count)
   })
   
 })
