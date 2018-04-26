ffe5b059b366a53ac419406f4c5dcdb1c85be811
Thompson D.Y <thompsonjanely@gmail.com>
2018-04-27 01:51:34 +0700

    Update app.r

--- app.r
+++ app.r
@@ -1,11 +1,31 @@
+#Still currently in testing and not in any way finished
+
+#When using this for your own use it's better to retype it than copy and paste so i highly suggest it
+
+#Before you do anything you will need to install a few packages in order for things to work
+#All you need to do is type these thing's below (Without the #) in the R console
+#install.packages("shiny")
+#install.packages("shinyjs)
+#install.packages("leaflet")
+#install.packages("jsonlite")
+#install.packages("shinydashboard")
+
+#You will also need to load the libraries by typing these in the console then you will finally be able to use it
 library(shiny)
 library(shinyjs)
 library(leaflet)
 library(jsonlite)
 library(shinydashboard)
 
-
 #UI-------------------------------------------------------------------------------------------------------------------------------------
+#Sets up the descriptions in the UI
+#You can change the parameters
+
+#the includeCSS is not a note, it will be needed to be there for things to run
+#Title is self explanatory
+#sliderInput is how much tracked items do you want to see (The 3 number parameters by order is minimum,maximum,default)
+#Leaflet creates the map
+
 ui <- fluidPage( 
   #includeCSS("styles.css"),
   dashboardHeader(title = "Live tracker"),
@@ -14,25 +34,46 @@ ui <- fluidPage(
 )
 
 #Server---------------------------------------------------------------------------------------------------------------------------------
-server <- function(input, output, session)
+#
+server <- function(input, output, session) 
 {
-library(jsonlite)
-jsonFile = fromJSON("http://api.metro.net/agencies/lametro/vehicles/")
-  dataFrame <- as.data.frame(jsonFile)
-  newDataFrame <- dataFrame[c(3,4,6,7)]
+  #jsonlite loaded again to make sure
+  library(jsonlite)
   
+  #jsLandTransport will read from a json in a website and then landTransport will take jsLandTransport's information and put it into
+  #a data frame, the website json that is used for this is written below (The parameter for the website cannot be changed unless you
+  #make adjustment to the code)
+   jsonFile = fromJSON("http://api.metro.net/agencies/lametro/vehicles/")
+  dataFrame <- as.data.frame(jsonFile)
+  vehicleDataFrame <- dataFrame[c(3,4,6,7)]
+	
   jsonFile2 = fromJSON("http://api.metro.net/agencies/lametro/routes/704/stops/")
   dataFrame2 <- as.data.frame(jsonFile2)
   stopsDataFrame <- dataFrame2[c(1,2,3)]
   
   redMarker <- makeIcon(iconUrl = "https://www.iconsdb.com/icons/download/red/map-marker-2-16.png")
   greenMarker <- makeIcon(iconUrl = "https://www.iconsdb.com/icons/download/green/map-marker-2-16.png")
-    output$map <- renderLeaflet
-({
-      leaflet(data = vehicleDataFrame[1:input$count,]) %>% addTiles() %>%
+  #Displaying the content of landTransport
+  #landTransport
+  
+  #Yes
+  values <- reactiveValues()
+  
+  #This will make the map display showing the locations and details on each tracked land transports
+  output$mymap <- renderLeaflet({
+   leaflet(data = vehicleDataFrame[1:input$count,]) %>% addTiles() %>%
       addAwesomeMarkers(~items.longitude, ~items.latitude, icon=greenMarker, popup = ~as.character(items.heading), label=~as.character(items.id))
-	    leaflet(data = stopsDataFrame[1:input$count,]) %>% addTiles() %>%
+	leaflet(data = stopsDataFrame[1:input$count,]) %>% addTiles() %>%
       addAwesomeMarkers(~items.longitude, ~items.latitude, icon=redMarker, popup = ~as.character(items.route_id), label=~as.character(items.route_id)) 
+  
+  #Observes the API and make changes
+  observe({
+    invalidateLater(1000, session)
+    dataFrame = fromJSON("http://api.metro.net/agencies/lametro/vehicles/")
+    dataFrame2 = fromJSON("http://api.metro.net/agencies/lametro/routes/704/stops/")
   })
+   
+})
+
 }
 shinyApp(ui, server)
