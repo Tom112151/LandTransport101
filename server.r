c991dd46a1efd6a9cee9fee37090973a85edbf1a
Andreas001 <andreasgeraldo0@gmail.com>
2018-04-26 23:44:01 +0700

    Update server.r

--- server.r
+++ server.r
@@ -1,7 +1,20 @@
 library(shiny)
 library(shinydashboard)
+library(leaflet)
+library(jsonlite)
 
 shinyserver(function(input, output)
 {
+  library(jsonlite)
+  
+  jsonFile = fromJSON("http://api.metro.net/agencies/lametro/vehicles/")
+  dataFrame <- as.data.frame(jsonFile)
+  newDataFrame <- dataFrame[c(3,4,6,7)]
+  
+  output$map <- renderLeaflet
+  ({
+      leaflet(data = newDataFrame[1:input$count,]) %>% addTiles() %>%
+      addMarkers(~items.longitude, ~items.latitude, popup = ~as.character(items.heading), label=~as.character(items.id)) 
+  })
   
 })
